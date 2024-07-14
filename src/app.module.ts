6979bb10186f43185a3bec3046eff0a83b05e3a7
Debasish Debnath <ddebnath241@gmail.com>
2024-07-14 23:21:14 +0530

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [AppController],
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'system',
+      password: 'debasish',
+      database: 'nestjs',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
