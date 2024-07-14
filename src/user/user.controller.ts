d36a16aba56dff800d23e0243f20c2b12c904a34
Debasish Debnath <ddebnath241@gmail.com>
2024-07-14 19:08:00 +0530

    use user service to delete,get,patch,update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,30 +8,40 @@ import {
   Req,
 } from '@nestjs/common';
 import { Request } from 'express';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+//   private userService;
+//   constructor() {
+//     //     this.userService = userSerrvice;
+//     //or
+//     this.userService = new UserService();
+//   }
+    constructor(private userService: UserService) {}
+
   @Get()
   getUsers() {
-    return { name: 'Debasish Debnath', email: 'ddebnath241@gmail.com' };
+    return this.userService.get();
+    // return { name: 'Debasish Debnath', email: 'ddebnath241@gmail.com' };
   }
 
   @Post()
   store(@Req() req: Request) {
-    return req.body;
+    return this.userService.create(req);
   }
   @Patch()
-  update(@Req() req: Request) {
-    return req.body;
+  update(@Req() req: Request, @Param() param:{userId: number}) {
+    return this.userService.update(req,param)
   }
 
   @Get('/:userId')
-  getUser(@Param() params: { userId: number }) {
-    return params;
+  getUser(@Param() param: { userId: number }) {
+    return this.userService.getUser(param);
   }
 
   @Delete('/:userId')
-  deleteUser(@Param() params: { userId: number }) {
-    return params;
+  deleteUser(@Param() param: { userId: number }) {
+    return this.userService.delete(param);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  get() {
+    return { name: 'Debasish Debnath', email: 'ddebnath241@gmail.com' };
+  }
+
+  create(req: Request) {
+    return req.body;
+  }
+  
+  update(req: Request, param: { userId: number }) {
+    return { body: req.body, param };
+  }
+
+  getUser(param: { userId: number }) {
+    return param;
+  }
+  delete(param: { userId: number }) {
+    return param;
+  }
+}
