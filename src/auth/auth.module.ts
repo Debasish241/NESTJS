32272388674d00f2a97b4637a75d062b03628aac
DebasishDebnath <debasish.debnath@iemlabs.com>
2024-07-15 16:22:56 +0530

    implement jwt token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,21 @@ import { UserModule } from 'src/user/user.module';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './auth.constant';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  imports: [UserModule,PassportModule],
-  providers: [AuthService,LocalStrategy],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy,JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
