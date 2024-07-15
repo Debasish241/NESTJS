bb22bed4db7e460eb09c5fe3f9b62a6629f7651a
DebasishDebnath <debasish.debnath@iemlabs.com>
2024-07-15 15:35:19 +0530

    add passport authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,24 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
+import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private userService: UserService){}
-
-    @Post('/login')
-    async login(@Body() loginDto: any){
-        
-        const user = await this.userService.findByEmail(loginDto.email);
-        if(user){
-            if(user.password === loginDto.password){
-            return user;
-        }
-
-        return "password doesn't Match"
-    }
-
-
-    
-        return 'unAuthenticated';
-    }
+  @UseGuards(AuthGuard('local'))
+  @Post('/login')
+  async login(@Request() req: any) {
+    return req.user;
+  }
 }
